c79a3b86f12fe8b13e4df7b06723cdabecff333b
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 22:55:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,15 @@
+require "pry"
 def my_collect(languages)
   i = 0
   while i < languages.length
     yield(languages[i])
     i += 1
+    binding.pry
   end
 end
 
-my_collect(["ruby", "javascript", "python", "objective-c"]) do |language| language.upcase 
-end
+# my_collect(["ruby", "javascript", "python", "objective-c"]) do |language| language.upcase 
+# end
 
 
 # my_collect(["ruby", "javascript", "python", "objective-c"]) { |languages| "#{languages}" }
