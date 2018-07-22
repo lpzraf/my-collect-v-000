5ba0ceef0d6ffb5f7ef98303424844ab770c88ec
Rafael Lopez <lpz.raf@gmail.com>
2018-07-22 23:03:55 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,8 +8,8 @@ def my_collect(languages)
   end
 end
 
-my_collect(["ruby", "javascript", "python", "objective-c"]) do |language| language.upcase 
-end
+my_collect(["ruby", "javascript", "python", "objective-c"])
+
 
 
 
